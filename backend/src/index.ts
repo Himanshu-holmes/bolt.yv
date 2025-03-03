78ef4d579db03ab463f7dd072da44666370e46cf
heeman <himanshuholmes@gmail.com>
2025-03-03 16:17:14 +0530

    update backend configuration: enhance .gitignore, add express and types, implement base prompt and stripIndents utility

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,23 @@
+import { getSystemPrompt } from "./prompts";
+
 require("dotenv").config();
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+import express from "express"
 
+
+const app = express()
+app.use(express.json())
+app.post("/template",(req,res)=>{
+ const prompt
+})
 
 async function main(){
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+    const model = genAI.getGenerativeModel({
+      model: "gemini-2.0-flash",
+      systemInstruction: getSystemPrompt(),
+    });
     
     const prompt = "Write the code for a TODO application";
     

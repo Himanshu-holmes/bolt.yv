d6b6dc61a12bf5c2ed87a6f21692cb7ee0d1017f
heeman <himanshuholmes@gmail.com>
2025-03-04 11:53:40 +0530

    complete template route

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -15,13 +15,58 @@ var __asyncValues = (this && this.__asyncValues) || function (o) {
     function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
     function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const prompts_1 = require("./prompts");
 require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const express_1 = __importDefault(require("express"));
+const node_1 = require("./defaults/node");
+const react_1 = require("./defaults/react");
+const prompts_2 = require("./prompts");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const prompt = req.body.prompt;
+    const model = yield genAI.getGenerativeModel({
+        model: "gemini-2.0-flash",
+        systemInstruction: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra",
+    });
+    const response = yield model.generateContent(prompt);
+    //  console.log("model response",response)
+    const answer = (yield response.response.text()).trim(); // react or node
+    console.log("answer", answer);
+    if (answer === "react") {
+        res.json({
+            prompts: [
+                prompts_2.BASE_PROMPT,
+                `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+            ],
+            uiPrompts: [react_1.basePrompt],
+        });
+        return;
+    }
+    if (answer === "node") {
+        res.json({
+            prompts: [
+                `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${node_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+            ],
+            uiPrompts: [node_1.basePrompt],
+        });
+        return;
+    }
+    res.status(403).json({ message: "You can't access this" });
+}));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         var _a, e_1, _b, _c;
-        const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+        const model = genAI.getGenerativeModel({
+            model: "gemini-2.0-flash",
+            systemInstruction: (0, prompts_1.getSystemPrompt)(),
+        });
         const prompt = "Write the code for a TODO application";
         const result = yield model.generateContentStream(prompt);
         try {
@@ -42,4 +87,7 @@ function main() {
         }
     });
 }
-main();
+// main();
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,16 +4,52 @@ require("dotenv").config();
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 import express from "express"
+import { basePrompt as nodeBasePrompt } from "./defaults/node";
+import { basePrompt as reactBasePrompt } from "./defaults/react";
+import { BASE_PROMPT } from "./prompts";
 
 
 const app = express()
 app.use(express.json())
-app.post("/template",(req,res)=>{
- const prompt
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+app.post("/template",async(req,res)=>{
+ const prompt = req.body.prompt
+
+   const model = await genAI.getGenerativeModel({
+     model: "gemini-2.0-flash",
+     systemInstruction:
+       "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra",
+   });
+   const response = await model.generateContent(prompt);
+//  console.log("model response",response)
+     const answer =  (await response.response.text() as string).trim(); // react or node
+   console.log("answer",answer)
+     if (answer === "react") {
+       res.json({
+         prompts: [
+           BASE_PROMPT,
+           `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+         ],
+         uiPrompts: [reactBasePrompt],
+       });
+       return;
+     }
+
+     if (answer === "node") {
+       res.json({
+         prompts: [
+           `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+         ],
+         uiPrompts: [nodeBasePrompt],
+       });
+       return;
+     }
+
+     res.status(403).json({ message: "You can't access this" });
 })
 
 async function main(){
-    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({
       model: "gemini-2.0-flash",
       systemInstruction: getSystemPrompt(),
@@ -30,4 +66,8 @@ async function main(){
 
 }
 
-main();
+// main();
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})  
